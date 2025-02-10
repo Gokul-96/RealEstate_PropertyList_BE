2662f28747f4b5905c529ae1de2f6b0c9f5aef2e
gokul <gokulakrishnan.m96@gmail.com>
2025-02-10 10:50:28 +0530

    DB initiated

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
 const express = require('express');
-const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const cors = require('cors');
+const connectDB = require('./config/db');
 const propertyRoutes = require('./routes/propertyRoutes')
+
+
+dotenv.config();
+//Connect MongoDB
+connectDB();
+
+
 //create app
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+
 app.use('/', propertyRoutes);
 
-
-const PORT = 8000;
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
